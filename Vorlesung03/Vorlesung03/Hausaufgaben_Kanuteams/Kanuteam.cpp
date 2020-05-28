56f280571a6a47a6fad7b7211a49b562df234c96
LucRome <64947931+LucRome@users.noreply.github.com>
2020-05-28 22:22:02 +0200

    Vorlesung03/HA_Kanuteams: Ausgabefunktion Kanuteam bearbeitet, set Funktion für m_members hinzugefügt (Kanuteam)

--- Vorlesung03/Vorlesung03/Hausaufgaben_Kanuteams/Kanuteam.cpp
+++ Vorlesung03/Vorlesung03/Hausaufgaben_Kanuteams/Kanuteam.cpp
@@ -1,4 +1,6 @@
 #include "Kanuteam.h"
+#include "Boat.h"
+#include "Person.h"
 #include <iostream>
 
 Kanuteam::Kanuteam(std::string name, Boat* boat)
@@ -15,7 +17,23 @@ void Kanuteam::printInfos() const
 {
 	std::cout << "----------------" << std::endl << "Kanuteam: " << std::endl;
 	IdAndName::printInfos();
-	//Noch Boot und Mitglieder ausgeben//
+	if (m_boat) {
+		std::cout << "Kanu: " << m_boat->getName << std::endl;
+	}
+	std::cout << "Members: " << std::endl;
+	for(Person * const person : m_members) {
+		std::cout << person->getName();
+	}
+}
+
+void Kanuteam::setMembers(std::vector<Person*> members)
+{
+	if (members.size <= max_Members) {
+		m_members = members;
+	}
+	else {
+		std::cout << "Zu viele Mitglieder" << std::endl;
+	}
 }
 
 void Kanuteam::addMember(Person* person)
--- Vorlesung03/Vorlesung03/Hausaufgaben_Kanuteams/Kanuteam.h
+++ Vorlesung03/Vorlesung03/Hausaufgaben_Kanuteams/Kanuteam.h
@@ -13,6 +13,7 @@ public:
 	Kanuteam(std::string name, Boat* boat = nullptr);
 	~Kanuteam() override;
 	void printInfos()  const override;
+	void setMembers(std::vector<Person*>);
 	void addMember(Person* person); //mit alias?
 	void addBoat(Boat* boat);
 
